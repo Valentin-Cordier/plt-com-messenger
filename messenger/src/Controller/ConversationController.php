59356d92802cb958be97340eb801f2984dedacd3
Valentin-Cordier <v.cordier@simplon-charleville.fr>
2019-09-05 16:04:08 +0200

    envoi de message (pas complètement opérationnel)

--- messenger/src/Controller/ConversationController.php
+++ messenger/src/Controller/ConversationController.php
@@ -3,7 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\MessagePri;
+use App\Form\MessagePriType;
+use App\Repository\MessagePriRepository;
 use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,30 +20,110 @@ class ConversationController extends AbstractController
      */
     public function index(UserRepository $repo)
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $user = $this->getUser();
         $repo = $this->getDoctrine()->getRepository(User::class);
-
+        $username->setIdUser($this->getUser());
         $username = $repo->findAll();
 
-        return $this->render('conversation/index_conv.html.twig', [
+
+        return $this->render('conversation/conversation.html.twig', [
             'users' => $username
-        ]);
+        ]);  
+            
     }
+
+    //      /**
+    //   * Permet l'affiche de la conversation avec l'utilisateur après un envoye de message
+    //   * 
+    //   * @Route("/conversation_show/{id}", name="envoi_message")
+    //   * 
+    //   */
+    //   public function envoi_message(Request $request,ObjectManager $manager)
+    //   {
+    //      $message = new MessagePri();
+ 
+    //      $form = $this->createForm(MessagePriType::class, $message);
+ 
+    //      $form->handleRequest($request);
+ 
+    //      if($form->isSubmitted()) {
+ 
+    //         $em = $this->getDoctrine()->getManager();
+            
+    //         $em->persist($message);
+ 
+    //         $em->flush();
+ 
+    //         return new Response('Message envoyé !');
+    //      }
+ 
+    //      return $this->render('conversation_show.html.twig', [
+    //          'form' => $form->createView(),
+    //          'message' => $message
+    //      ]);
+         
+    //   }
 
     /**
      * Permet l'affiche de la conversation avec l'utiilisateur
      * 
-     * @Route("/conversation_show/{id}", name="conversation_show")
+     * @Route("/conversation/{id}", name="conversation_show")
      * 
      * @return Response
      */
-    public function show(UserRepository $repo)
+    public function show(Request $request, ObjectManager $manager, UserRepository $repo, MessagePriRepository $repo2, $id)
     {
-        $repo = $this->getDoctrine()->getRepository(User::class);
+        
+        $message = new MessagePri();
+
+        
+        $form = $this->createForm(MessagePriType::class, $message);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()) {
+
+           $em = $this->getDoctrine()->getManager();
+
+           $message->setIdUserRecevoir($this->getUser());
+           $message->setIdUser($this->getUser());
+
+           $em->persist($message);
+
+           $em->flush();
+
+           return new Response('Message envoyé !');
+
+           return $this->redirectToRoute('conversation_show', ['id' => $message->getIdUser()]);
+        }
+
+
+
+
+        $repo = $this->getDoctrine()->getEntityManager()->getRepository(User::class);
 
         $username = $repo->findAll();
 
+
+
+        
+
+
+
+        $repo2 = $this->getDoctrine()->getRepository(MessagePri::class);
+
+        
+         $mess = $repo2->findAll();
+
+
+
+        
+
         return $this->render('conversation/conversation_show.html.twig', [
-            'users' => $username
+            'form' => $form->createView(),
+            'users' => $username,   
+            'message' => $mess, 
         ]);
 
     }
